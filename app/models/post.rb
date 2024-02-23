616c820cd2924a85ecbfcc038c2e2cd67dc5b74a
Nikita Konovalov <mipo4t@gmail.com>
2024-02-23 17:40:36 +0400

    fix blog body chars count

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,7 +5,7 @@ class Post < ApplicationRecord
   MAX_TITLE_LENGTH = 250
 
   MIN_BODY_LENGTH = 200
-  MAX_BODY_LENGTH = 400
+  MAX_BODY_LENGTH = 4000
 
   belongs_to :category
   belongs_to :creator, class_name: 'User', foreign_key: 'user_id', inverse_of: 'posts'
