f16dc68b7d3ab0e8ebf9fca7e4b53cedc7b4dab1
Nikita Konovalov <mipo4t@gmail.com>
2024-02-29 00:04:12 +0400

    add likes to views

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,19 @@ class User < ApplicationRecord
   has_many :posts, inverse_of: 'creator', dependent: :destroy
   has_many :comments, class_name: 'PostComment', dependent: :destroy
   has_many :likes, class_name: 'PostLike', dependent: :destroy
+  has_many :liked_posts, through: :likes, source: :post
 
   devise :database_authenticatable, :registerable, :rememberable, :validatable
 
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP },
                     presence: true,
                     uniqueness: { case_sensitive: false }
+
+  def liked?(post)
+    liked_posts.include?(post)
+  end
+
+  def like_for(post)
+    likes.find_by(post:)
+  end
 end
