e592e9706d53721e4bfbc313846eba54ffa9b674
Nikita Konovalov <mipo4t@gmail.com>
2024-03-02 20:04:47 +0400

    fix: remove constants from Post model

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,4 +1,16 @@
 # frozen_string_literal: true
 
 module PostsHelper
+  def min_length_of_field(field)
+    length_validator_options_for(field)[:minimum]
+  end
+
+  def max_length_of_field(field)
+    length_validator_options_for(field)[:maximum]
+  end
+
+  def length_validator_options_for(field)
+    validator = Post.validators_on(field.to_sym).find { |v| v.kind == :length }
+    validator ? validator.options : {}
+  end
 end
